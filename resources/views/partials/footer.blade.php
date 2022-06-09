35a9fe8943edb8b24a803ffcd4a2085eba2c6bd7
robertolatini31 <robertolatini31@gmail.com>
2022-06-09 17:24:05 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,17 @@
 <footer>
-
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 align-items-center">
+            <div class="col">
+                <button class="text-uppercase text-white">sing-up now!</button>
+            </div>
+            <div class="col d-flex justify-content-end align-items-center flex-wrap">
+                <h3 class="text-uppercase m-0 p-1">follow us</h3>
+                <img class="p-1" src="{{asset('img/footer-facebook.png')}}" alt="fb">
+                <img class="p-1" src="{{asset('img/footer-twitter.png')}}" alt="tw">
+                <img class="p-1" src="{{asset('img/footer-youtube.png')}}" alt="yt">
+                <img class="p-1" src="{{asset('img/footer-pinterest.png')}}" alt="pinterest">
+                <img class="p-1" src="{{asset('img/footer-periscope.png')}}" alt="periscope">
+            </div>
+        </div>
+    </div>
 </footer>
