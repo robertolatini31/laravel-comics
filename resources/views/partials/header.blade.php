413aa887cc8c1f39635914c7cde33a183e48b2c5
robertolatini31 <robertolatini31@gmail.com>
2022-06-09 15:46:10 +0200

    add header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,64 @@
-<header>
-    <h2 class="text-center">header</h2>
-</header>
+<header id="site_header">
+    <div class="up_colored">
+        <div class="container">
+            <div class="d-flex align-items-center justify-content-end p-1 text-white">
+                    <span class="text-uppercase me-5">dc power visa&reg;</span>
+                    <span class="text-uppercase">additional dc sites</span>
+            </div>
+        </div>
+    </div>
+    <!-- /.up_colored -->
+    <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <img src="{{asset('img/dc-logo.png')}}" alt="logo_dc">
+            </div>
+       
+            <div class="col-8">
+            <nav>
+                <ul class="m-0 p-0">
+                    <li>
+                        <a class="text-uppercase">characters</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">comics</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">movies</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">tv</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">games</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">collectibles</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">videos</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">fans</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">news</a>
+                    </li>
+                    <li>
+                        <a class="text-uppercase">shop</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- ./ navbar  -->
+            </div>
+            <!--/.navbar_header  -->
+            <div class="col-2">
+            <input class="form-control border-0 rounded-0 text-end px-0" type="search" placeholder="Search" aria-label="Search">
+            </div>
+            
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+   </header>
+   <!-- /.SiteHeader -->
