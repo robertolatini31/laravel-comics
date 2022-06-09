241e7595167e9ef381f35c13b817b5703c7fa53c
robertolatini31 <robertolatini31@gmail.com>
2022-06-09 16:16:59 +0200

    add home_main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials.jumbotron')
-    <h1>main</h1>
+    <main id="site_main">
+        @include('partials.jumbotron')
+        <div class="container">
+            <h3 class="current_series">current series</h3>
+            <!-- /.current_series -->
+            <div class="container_cards pt-5">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 rowc-cols-lg-4 row-cols-xl-5 row-cols-xxl-6">
+                @forelse($books as $book)
+                    <div class="col">  
+                        <div class="my_card">
+                            <a href="#">
+                                <img src="{{$book['thumb']}}" alt="book_img">
+                            </a>
+                            <h4>{{$book['series']}}</h4>
+                            <p>{{$book['price']}}</p>
+                            <p>{{$book['type']}}</p>
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                @empty
+                    <p>no books</p>
+                @endforelse
+                </div>
+            </div>
+            <!-- /.container_cards -->
+
+            <div class="btn_container">
+                <button>load more</button>
+            </div>
+            <!-- /.btn_container -->
+            
+        </div>
+        <!-- /.container -->
+    </main>
+    
+    
 @endsection
