ce2224296d4c0909b37e1fc047ac63e4628402dd
robertolatini31 <robertolatini31@gmail.com>
2022-06-10 15:06:11 +0200

    add book_show img section

--- resources/views/book_show.blade.php
+++ resources/views/book_show.blade.php
@@ -4,7 +4,25 @@
 
 @include('partials.jumbotron')
 
+<main>
 
+    <div class="single_book_img_section py-4">
+        <div class="container">
+            <div class="container_relative position-relative">
+                <div class="container_absolute position-absolute">
+                    <div class="card_img d-inline-block position-relative border border-white">
+                        <img src="{{$book['thumb']}}" alt="book_img">
+                        <span class="type_text text-white text-uppercase position-absolute p-1">{{$book['type']}}</span>
+                        <div class="gallery_text text-white text-uppercase position-absolute p-1 text-center">view gallery</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+
+</main>
 
 @include('partials.menu_section')
 
